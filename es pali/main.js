d923cdce73c50576b67b9d764f36600f0becde3b
Hemil78 <zanattahemil@gmail.com>
2021-09-01 20:36:19 +0200

    Palindroma

--- es pali/main.js
+++ es pali/main.js
@@ -1,6 +1,34 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+// scrivo una funzione
+function isPalindrome(str) {
+    var strReverse = "";
+    for( var i = str.length - 1; i >= 0; i--) {
+        strReverse += str[i];
+    }
+    // confronto la parola capovolta con la parola originale
+    if(str == strReverse) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+
+
+// chiedo all'utente di inserire una parola
+var parola = prompt("Inserisci una parola");
+
+if( isPalindrome( parola ) == true ) {
+    alert( "Complimenti, la tua parola è Palindroma! Hai vinto un I Phone" );
+} else {
+    alert( "Mi spiace! la tua parola non è Palindroma. Riprova!" );
+}
+
+
+
 
 
 
